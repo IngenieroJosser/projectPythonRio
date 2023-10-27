bddbe495aa9ce754a4e55f66faaa9c12e62933b4
IngenieroJosser <133165583+IngenieroJosser@users.noreply.github.com>
2023-10-26 23:20:09 -0700

    First Commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import RegisterHere from './log/RegisterHere'
+import Footer from './log/Footer'
+
+const Login = () => {
+    return (
+        <>
+            <RegisterHere />
+            <Footer />
+        </>
+    )
+}
+
+export default Login
---
+++ src/components/log/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../../scss/log/_Footer.scss'
+
+const Footer = () => {
+    return (
+        <>
+            <footer>
+                <span>¿Preguntas? Llama al <p> 01 800 917 1564</p></span>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
