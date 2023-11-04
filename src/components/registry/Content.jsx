96998d5bc12f2f7793f72a23de7ef25ba4b5cd8e
IngenieroJosser <133165583+IngenieroJosser@users.noreply.github.com>
2023-11-04 00:36:48 -0700

    ...

--- src/components/registry/Content.jsx
+++ src/components/registry/Content.jsx
@@ -6,10 +6,7 @@ const Content = () => {
     return (
         <>
             <div className="content">
-                <p>PASO <strong>1</strong> DE <strong>3</strong></p>
-                <h1>¡Hola de nuevo!</h1>
-                <h1 className='Subscribe'>Suscribirte a Netflix es fácil.</h1>
-                <span className='password'>Ingresa tu contraseña para comenzar a ver al instante.</span>
+                
             </div>
         </>
     )
