25330dbae0ad872492e6389a7e6764b24b0ee67f
Luis Carlos Chaverra Cordoba <lucas.cordoba1510@gmail.com>
2023-11-05 23:10:26 -0500

    Revert "actualizacion de rutas"
    This reverts commit d2caefa0f71754191875010da9bd48456e8a1d04.

--- src/components/mainSection/Question.jsx
+++ src/components/mainSection/Question.jsx
@@ -12,7 +12,7 @@ const Question = ({title,description}) => {
     <>
         <div className="question" onClick={handleclick}>
             <span>{title}</span>
-            <svg width="16" height="16" viewBox="0 0 36 16" fill="none" xmlns="http://www.w3.org/2000/svg" className="elj7tfr3 e164gv2o4 default-ltr-cache-l7vm9h e1svuwfo1" data-name="Plus" alt=""><path fillRule="evenodd" clipRule="evenodd" d="M17 17V3H19V17H33V19H19V33H17V19H3V17H17Z" fill="currentColor"></path></svg>
+            <svg width="16" height="16" viewBox="0 0 36 16" fill="none" xmlns="http://www.w3.org/2000/svg" class="elj7tfr3 e164gv2o4 default-ltr-cache-l7vm9h e1svuwfo1" data-name="Plus" alt=""><path fill-rule="evenodd" clip-rule="evenodd" d="M17 17V3H19V17H33V19H19V33H17V19H3V17H17Z" fill="currentColor"></path></svg>
         </div>
         <div className={`question-dropdown ${(clicked)? 'show': ' '}`}>
             <p>
--- src/main.jsx
+++ src/main.jsx
@@ -1,46 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider,} from "react-router-dom";
-import Login from './components/Login'
+// import Login from './components/Login'
 // import HeaderLog from './components/registry/HeaderLog'
-import Content from './components/registry/Content';
-import Signup from './components/registry/Signup';
-import PaymentPicker from './components/registry/PaymentPicker';
+// import Content from './components/registry/Content';
+// import Signup from './components/registry/Signup';
+// import PaymentPicker from './components/registry/PaymentPicker';
 import MainSection from './components/mainSection/MainSection';
-import UserProfile from './components/Account/UserProfile';
+// import UserProfile from './components/Account/UserProfile';
 
 import './css/index.css';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainSection />,
-  },
-  {
-    path: "/Profile",
-    element: <UserProfile />,
-  },
-  {
-    path: "/Login",
-    element: <Login/>,
-  },
-  {
-    path: "/Content",
-    element: <Content />,
-  },
-  {
-    path: "/Signup",
-    element: <Signup />,
-  },
-  {
-    path: "/PaymentPicker",
-    element: <PaymentPicker />,
-  },
-]);
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    {/* <Login /> */}
+    {/* <HeaderLog /> */}
+    {/* <Content /> */}
+    {/* <Signup /> */}
+    {/* <PaymentPicker /> */}
+    <MainSection />
+    {/* <UserProfile /> */}
   </React.StrictMode>,
 )
