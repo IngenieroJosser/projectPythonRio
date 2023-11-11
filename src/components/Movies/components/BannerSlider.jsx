3284fe8e82897d41e9cc4d0d971523fbd92b72f1
IngenieroJosser <133165583+IngenieroJosser@users.noreply.github.com>
2023-11-11 13:06:13 -0800

    ...

--- src/components/Movies/components/BannerSlider.jsx
+++ src/components/Movies/components/BannerSlider.jsx
@@ -176,8 +176,13 @@ const BannerSlider = () => {
                 </div>
 
                 <div className="number4">
-                    <svg id="rank-4" width="38%" height="38%" viewBox="0 0 81 154" class="svg-icon svg-icon-rank-4 top-10-rank"><path stroke="#595959" stroke-linejoin="square" stroke-width="4" d="M72 152h35.333v-30.977H128V92.497h-20.667V2H69.89L2 92.712v28.311h70V152zM36.202 92.188l35.93-47.998v47.998h-35.93z"></path></svg>
+                    <svg id="rank-4" width="38%" height="38%" viewBox="0 0 81 154"><path stroke="#595959" stroke-linejoin="square" stroke-width="4" d="M72 152h35.333v-30.977H128V92.497h-20.667V2H69.89L2 92.712v28.311h70V152zM36.202 92.188l35.93-47.998v47.998h-35.93z"></path></svg>
                     <img className='populares' src={popular4} alt="" />
+                </div>
+
+                <div className="number5">
+                    <svg id="rank-4" width="38%" height="38%" viewBox="0 0 81 154"><path stroke="#595959" stroke-linejoin="square" stroke-width="4" d="M72 152h35.333v-30.977H128V92.497h-20.667V2H69.89L2 92.712v28.311h70V152zM36.202 92.188l35.93-47.998v47.998h-35.93z"></path></svg>
+                    <img className='populares' src={popular5} alt="" />
                 </div>
             </div>
         </div>
